8903a92faafee0e78e59d26c26cf6a8434c30d37
Wizard1024 <wizard1024@gmail.com>
2018-03-27 11:10:05 +0300

    Algo with overlimit and main parent queue limit

--- opt/dmj/lib/lua-resty-rate-limit/lib/resty/rate/limit.lua
+++ opt/dmj/lib/lua-resty-rate-limit/lib/resty/rate/limit.lua
@@ -10,25 +10,71 @@ local function expire_key(redis_connection, key, interval, log_level)
     end
 end
 
-local function bump_request(redis_connection, redis_pool_size, ip_key, r_i, rateTotal, intervalTotal, current_time, log_level)
-    local key = "RL:" .. ip_key
-    local keyTotal = "RL:Total"
-    for i,r_i_keys in ipairs(r_i) do
-	if r_i_keys[1] == ip_key then
-		rate = r_i_keys[2]
-		interval = r_i_keys[3]
+local function get_key(redis_connection, key, log_level)
+	local value, error = redis_connection:get(key)
+	if not value then
+		ngx.log(log_level, "Failed to get key: ", error)
+		return
 	end
-    end
+	return value
+end
+
+local function set_lock(redis_connection)
+	local lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "1000", "NX")
+	while lock == ngx.null do
+		lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "1000", "NX")
+	end
+end
+
+local function unset_lock(redis_connection)
+	redis_connection:del("RL:Lock")
+end
+
+local function overlimit(redis_connection, ip_key, rate_max, interval, log_level)
+	local result = false
+    	local key = "RL:" .. ip_key
+	local keyTotal = "RL:Total"
+	local lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "10000", "NX")
+	while lock == ngx.null do
+		lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "10000", "NX")
+	end
+	--set_lock(redis_connection)
+	local total, error = redis_connection:incr(keyTotal)	
+	redis_connection:del("RL:Lock")
+	if not total then
+		ngx.log(log_level, "Failed to get key: ", error)
+	end
+	if tonumber(total) == 1 then 
+		result = false
+		expire_key(redis_connection, keyTotal, interval, log_level)
+	else
+		if tonumber(total) > rate_max then
+			result = true
+			redis_connection:decr(keyTotal)
+		else 
+			result = false
+       			local ttl, error = redis_connection:pttl(keyTotal)
+       			if not ttl then
+       			    ngx.log(log_level, "failed to get ttl: ", error)
+       			    return
+       			end
+       			if ttl == -2 then
+       			    ttl = interval
+       			    expire_key(redis_connection, keyTotal, interval, log_level)
+       			end
+		end
+	end
+	--redis_connection:del("RL:Lock")
+	--unset_lock(redis_connection)
+	return result
+end
+
+local function bump_request(redis_connection, redis_pool_size, ip_key, rate, interval, current_time, log_level)
+    local key = "RL:" .. ip_key
 
     local count, error = redis_connection:incr(key)
     if not count then
         ngx.log(log_level, "failed to incr count: ", error)
-        return
-    end
-
-    local countTotal, error = redis_connection:incr(keyTotal)
-    if not countTotal then
-        ngx.log(log_level, "failed to incr countTotal: ", error)
         return
     end
 
@@ -48,35 +94,12 @@ local function bump_request(redis_connection, redis_pool_size, ip_key, r_i, rate
         reset = (current_time + (ttl * 0.001))
     end
 
-    if tonumber(countTotal) == 1 then
-        resetTotal = (current_time + intervalTotal)
-        expire_key(redis_connection, keyTotal, intervalTotal, log_level)
-    else
-        local ttlTotal, error = redis_connection:pttl(keyTotal)
-        if not ttlTotal then
-            ngx.log(log_level, "failed to get ttl: ", error)
-            return
-        end
-        if ttlTotal == -2 then
-            ttlTotal = intervalTotal
-            expire_key(redis_connection, keyTotal, intervalTotal, log_level)
-        end
-        resetTotal = (current_time + (ttlTotal * 0.001))
-    end
-
-    local ok, error = redis_connection:set_keepalive(60000, redis_pool_size)
-    if not ok then
-        ngx.log(log_level, "failed to set keepalive: ", error)
-    end
-
     local remaining = rate - count
-    local remainingTotal = rateTotal - countTotal
 
-    return { count = count, remaining = remaining, reset = reset, countTotal = countTotal, remainingTotal = remainingTotal, resetTotal = resetTotal }
+    return { count = count, remaining = remaining, reset = reset }
 end
 
 function _M.limit(config)
-    local uri_parameters = ngx.req.get_uri_args()
     local use_limit = true
 
     if use_limit then
@@ -105,16 +128,16 @@ function _M.limit(config)
             end
 
             config.redis_config = redis_config
+	    -- Save established connection to config for use
             config.connection = redis_connection
         end
 
-        local current_time = ngx.now()
         local connection = config.connection
         local redis_pool_size = config.redis_config.pool_size
         local key = config.key or ngx.var.remote_addr
 	local rateTotal = config.rateTotal or 100;
 	local intervalTotal = config.intervalTotal or 1;
-	local r_i = config.r_i or {{"0", 10, 1}}
+	local r_i = config.r_i or {{"0", 10, 1, false}}
 	local rateTotalDec = rateTotal
 	local rate = 10
 	local interval = 1
@@ -124,64 +147,121 @@ function _M.limit(config)
     	    	rate = r_i_keys[2]
     	    	interval = r_i_keys[3]
 		enforce_limit = r_i_keys[4]
-		--if r_i_keys[4] == "true" then
-		--	enforce_limit = true
-		--else
-		--	enforce_limit = false
-		--end
     	    end
 	    rateTotalDec = rateTotalDec - 1;
     	end
 
-        local response, error = bump_request(connection, redis_pool_size, key, r_i, rateTotal, intervalTotal, current_time, log_level)
+        local current_time = ngx.now()
+        local response, error = bump_request(connection, redis_pool_size, key, rate, interval, current_time, log_level)
         if not response then
             return
         end
 
         if response.count > rate then
-		if rateTotalDec > response.remainingTotal then
-			min = response.remainingTotal
-		else
-			min = rateTotalDec
-		end
-		if enforce_limit and response.count < min then 
-	            ngx.header["X-RateLimit-Limit"] = rate
-	            ngx.header["X-RateLimit-Remaining"] = math.floor(response.remaining)
-	            ngx.header["X-RateLimit-Reset"] = math.floor(response.reset)
-	            ngx.header["X-RateLimit-Limit-Total"] = rateTotalDec
-	            ngx.header["X-RateLimit-Remaining-Total"] = math.floor(response.remainingTotal)
-	            ngx.header["X-RateLimit-Reset-Total"] = math.floor(response.resetTotal)
-		    if enforce_limit then
-			ngx.header["X-RateLimit-Enforce-Limit"] = "true"
-		    else
-			ngx.header["X-RateLimit-Enforce-Limit"] = "false"
-		    end
-		else	            
-	            local retry_after = math.floor(response.reset - current_time)
-	            if retry_after < 0 then
-	                retry_after = 0
-	            end
-	
-	            ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
-	            ngx.header["Access-Control-Allow-Origin"] = "*"
-	            ngx.header["Content-Type"] = "application/json; charset=utf-8"
-	            ngx.header["Retry-After"] = retry_after
-	            ngx.status = 429
-	            ngx.say('{"status_code":25,"status_message":"Your request count (' .. response.count .. ') is over the allowed limit of ' .. rate .. '."}')
-	            ngx.exit(ngx.HTTP_OK)
+		if enforce_limit then
+			if overlimit(connection, key, rateTotalDec, intervalTotal, log_level) then 
+	            		local retry_after = math.floor(response.reset - current_time)
+	            		if retry_after < 0 then
+	            		    retry_after = 0
+	            		end
+	            		ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
+	            		ngx.header["Access-Control-Allow-Origin"] = "*"
+	            		ngx.header["Content-Type"] = "application/json; charset=utf-8"
+	            		ngx.header["Retry-After"] = retry_after
+	            		ngx.status = 429
+	            		ngx.say('{"status_code":25,"status_message":"Your request count (' .. response.count .. ') is over the allowed limit of ' .. rate .. '."}')
+                    		--ngx.log(log_level, "OverLimit: ", response.count)
+				-- Return connection in pool
+    				local ok, error = connection:set_keepalive(60000, redis_pool_size)
+    				if not ok then
+    				    ngx.log(log_level, "failed to set keepalive: ", error)
+    				end
+	            		ngx.exit(ngx.HTTP_OK)
+            	 	else
+				--local responseTotal, error = bump_request(connection, redis_pool_size, "Total", r_i, current_time, log_level)
+				--if not responseTotal then
+				--    return
+				--end
+	        		ngx.header["X-RateLimit-Limit"] = rate
+	        		ngx.header["X-RateLimit-Remaining"] = math.floor(response.remaining)
+	        		ngx.header["X-RateLimit-Reset"] = math.floor(response.reset)
+	        		ngx.header["X-RateLimit-Limit-Total"] = rateTotalDec
+	        		--ngx.header["X-RateLimit-Remaining-Total"] = math.floor(responseTotal.remaining)
+            			--ngx.header["X-RateLimit-Count-Total"] = math.floor(responseTotal.count)
+	        		--ngx.header["X-RateLimit-Reset-Total"] = math.floor(responseTotal.reset)
+				--if enforce_limit then
+				--    ngx.header["X-RateLimit-Enforce-Limit"] = "true"
+				--else
+				--    ngx.header["X-RateLimit-Enforce-Limit"] = "false"
+				--end
+				-- Return connection in pool
+    				local ok, error = connection:set_keepalive(60000, redis_pool_size)
+    				if not ok then
+    				    ngx.log(log_level, "failed to set keepalive: ", error)
+    				end
+			end
+		else	
+	            	local retry_after = math.floor(response.reset - current_time)
+	            	if retry_after < 0 then
+	            	    retry_after = 0
+	            	end
+	            	ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
+	            	ngx.header["Access-Control-Allow-Origin"] = "*"
+	            	ngx.header["Content-Type"] = "application/json; charset=utf-8"
+	            	ngx.header["Retry-After"] = retry_after
+	            	ngx.status = 429
+	            	ngx.say('{"status_code":25,"status_message":"Your request count (' .. response.count .. ') is over the allowed limit of ' .. rate .. '."}')
+			-- Return connection in pool
+    			local ok, error = connection:set_keepalive(60000, redis_pool_size)
+    			if not ok then
+    			    ngx.log(log_level, "failed to set keepalive: ", error)
+    			end
+	            	ngx.exit(ngx.HTTP_OK)
 		end
         else
-            ngx.header["X-RateLimit-Limit"] = rate
-            ngx.header["X-RateLimit-Remaining"] = math.floor(response.remaining)
-            ngx.header["X-RateLimit-Reset"] = math.floor(response.reset)
-            ngx.header["X-RateLimit-Limit-Total"] = rateTotalDec
-            ngx.header["X-RateLimit-Remaining-Total"] = math.floor(response.remainingTotal)
-            ngx.header["X-RateLimit-Reset-Total"] = math.floor(response.resetTotal)
-            if enforce_limit then
-		ngx.header["X-RateLimit-Enforce-Limit"] = "true"
-	    else
-		ngx.header["X-RateLimit-Enforce-Limit"] = "false"
-	    end
+        	--local responseTotal, error = bump_request(connection, redis_pool_size, "Total", rateTotalDec, intervalTotal, current_time, log_level)
+		--unset_lock(connection)
+		--if not responseTotal then
+			--ngx.log(log_level, "Err: responseTotal: ", error)
+			--return
+		--end
+	    	if overlimit(connection, key, rateTotal, intervalTotal, log_level) then 
+	    		local retry_after = math.floor(response.reset - current_time)
+	    		if retry_after < 0 then
+	    		    retry_after = 0
+	    		end
+	    		ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
+	    		ngx.header["Access-Control-Allow-Origin"] = "*"
+	    		ngx.header["Content-Type"] = "application/json; charset=utf-8"
+	    		ngx.header["Retry-After"] = retry_after
+	    		ngx.status = 429
+	    		ngx.say('{"status_code":25,"status_message":"Your request count (' .. response.count .. ') is over the allowed limit of ' .. rate .. '."}')
+            		--ngx.log(log_level, "OverLimit: ", response.count)
+	        	-- Return connection in pool
+    	        	local ok, error = connection:set_keepalive(60000, redis_pool_size)
+    	        	if not ok then
+    	        	    ngx.log(log_level, "failed to set keepalive: ", error)
+    	        	end
+	    		ngx.exit(ngx.HTTP_OK)
+		else
+			ngx.header["X-RateLimit-Limit"] = rate
+			ngx.header["X-RateLimit-Remaining"] = math.floor(response.remaining)
+			ngx.header["X-RateLimit-Reset"] = math.floor(response.reset)
+			ngx.header["X-RateLimit-Limit-Total"] = rateTotalDec
+			--ngx.header["X-RateLimit-Remaining-Total"] = math.floor(responseTotal.remaining)
+			--ngx.header["X-RateLimit-Count-Total"] = math.floor(responseTotal.count)
+			--ngx.header["X-RateLimit-Reset-Total"] = math.floor(responseTotal.reset)
+			--if enforce_limit then
+			--    ngx.header["X-RateLimit-Enforce-Limit"] = "true"
+			--else
+			--    ngx.header["X-RateLimit-Enforce-Limit"] = "false"
+			--end
+			-- Return connection in pool
+			local ok, error = connection:set_keepalive(60000, redis_pool_size)
+			if not ok then
+			    ngx.log(log_level, "failed to set keepalive: ", error)
+			end
+		end
         end
     else
         return
