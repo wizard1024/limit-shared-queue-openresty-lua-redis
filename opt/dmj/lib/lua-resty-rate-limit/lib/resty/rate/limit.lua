149d4146b736b643ac19eaa9df940ffa15cdbd83
root <root@app-1.sdl-test.local>
2018-06-26 06:34:18 +0000

    Latest at 2018-06-26

--- opt/dmj/lib/lua-resty-rate-limit/lib/resty/rate/limit.lua
+++ opt/dmj/lib/lua-resty-rate-limit/lib/resty/rate/limit.lua
@@ -10,72 +10,113 @@ local function expire_key(redis_connection, key, interval, log_level)
     end
 end
 
--- Function to check Parent queue for limits
-local function overlimit(redis_connection, ip_key, rate_max, interval, log_level)
-	local result = false
-    	local key = "RL:" .. ip_key
-	local keyTotal = "RL:Total"
-	local lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "10000", "NX")
-	while lock == ngx.null do
-		lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "10000", "NX")
+local function ttl_key(redis_connection, key, interval, log_level)
+	local ttl, error = redis_connection:pttl(key)
+        if not ttl then
+        	ngx.log(log_level, "failed to get ttl: ", error)
+            	return
+        end
+        if ttl == -1 then
+            	expire_key(redis_connection, key, interval, log_level)
+        end
+	if ttl == -2 then
+		redis_connection:incr(key)
+		expire_key(redis_connection, key, interval, log_level)
 	end
-	local total, error = redis_connection:incr(keyTotal)	
-	redis_connection:del("RL:Lock")
-	if not total then
-		ngx.log(log_level, "Failed to get key: ", error)
-	end
-	if tonumber(total) == 1 then 
-		result = false
-		expire_key(redis_connection, keyTotal, interval, log_level)
-	else
-		if tonumber(total) > rate_max then
-			result = true
-			redis_connection:decr(keyTotal)
-		else 
-			result = false
-       			local ttl, error = redis_connection:pttl(keyTotal)
-       			if not ttl then
-       			    ngx.log(log_level, "failed to get ttl: ", error)
-       			    return
-       			end
-       			if ttl == -2 then
-       			    ttl = interval
-       			    expire_key(redis_connection, keyTotal, interval, log_level)
-       			end
-		end
-	end
-	return result
+		
 end
 
 -- Function to increse class queue and set TTL to key
-local function bump_request(redis_connection, redis_pool_size, ip_key, rate, interval, current_time, log_level)
-    local key = "RL:" .. ip_key
+local function bump_request_all(redis_connection, redis_pool_size, ip_key, rate, interval, rateTotal, rateTotalDec, intervalTotal, enforce_limit, log_level)
+    	local proceed=true
+    	local key = "RL:" .. ip_key
+	local keyTotal = "RL:Total"
+	local dec = 0
 
-    local count, error = redis_connection:incr(key)
-    if not count then
-        ngx.log(log_level, "failed to incr count: ", error)
-        return
-    end
+	--local lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "1000", "NX")
+	--while lock == ngx.null do
+	--	ngx.sleep(0.001)
+	--	lock, error = redis_connection:set("RL:Lock", "Locked", "PX", "1000", "NX")
+	--end
 
-    if tonumber(count) == 1 then
-        reset = (current_time + interval)
-        expire_key(redis_connection, key, interval, log_level)
-    else
-        local ttl, error = redis_connection:pttl(key)
-        if not ttl then
-            ngx.log(log_level, "failed to get ttl: ", error)
-            return
-        end
-        if ttl == -2 then
-            ttl = interval
-            expire_key(redis_connection, key, interval, log_level)
-        end
-        reset = (current_time + (ttl * 0.001))
-    end
+    	local count, error = redis_connection:incr(key)
+    	if not count then
+        	ngx.log(log_level, "failed to incr count: ", error)
+        	return
+    	end
+	local total, error = redis_connection:incr(keyTotal)	
+	if not total then
+		ngx.log(log_level, "Failed to get key: ", error)
+	end
+	--redis_connection:del("RL:Lock")
 
-    local remaining = rate - count
+	local itotal = tonumber(total)
+	if itotal == 1 then 
+		expire_key(redis_connection, keyTotal, interval, log_level)
+	end
 
-    return { count = count, remaining = remaining, reset = reset }
+	local icount = tonumber(count)
+    	if icount == 1 then
+        	expire_key(redis_connection, key, interval, log_level)
+    	else
+    		if enforce_limit then
+			if icount <= rate then
+				if itotal > rateTotal then
+					-- DECR
+					dec, error = redis_connection:decr(keyTotal)
+					if not dec then
+						ngx.log(log_level, "Failed to decr key:", error)
+					end
+					dec, error = redis_connection:decr(key)
+					if not dec then
+						ngx.log(log_level, "Failed to decr key:", error)
+					end
+					proceed = false
+				end
+			else 
+				if itotal > rateTotalDec then
+					-- DECR
+					dec, error = redis_connection:decr(keyTotal)
+					if not dec then
+						ngx.log(log_level, "Failed to decr key:", error)
+					end
+					dec, error = redis_connection:decr(key)
+					if not dec then
+						ngx.log(log_level, "Failed to decr key:", error)
+					end
+					proceed = false
+				end
+			end
+		else
+			if icount <= rate then
+				if itotal > rateTotal then
+					-- DECR
+					dec, error = redis_connection:decr(keyTotal)
+					if not dec then
+						ngx.log(log_level, "Failed to decr key:", error)
+					end
+					dec, error = redis_connection:decr(key)
+					if not dec then
+						ngx.log(log_level, "Failed to decr key:", error)
+					end
+					proceed = false
+				end
+			else 
+				-- DECR
+				dec, error = redis_connection:decr(keyTotal)
+				if not dec then
+					ngx.log(log_level, "Failed to decr key:", error)
+				end
+				dec, error = redis_connection:decr(key)
+				if not dec then
+					ngx.log(log_level, "Failed to decr key:", error)
+				end
+				proceed = false
+			end
+		end
+    	end
+
+    	return { proceed = proceed }
 end
 
 function _M.limit(config)
@@ -116,12 +157,14 @@ function _M.limit(config)
         local redis_pool_size = config.redis_config.pool_size
         local key = config.key or ngx.var.remote_addr
 	local rateTotal = config.rateTotal or 100;
+	--local rateTotal = 0
 	local intervalTotal = config.intervalTotal or 1;
 	local r_i = config.r_i or {{"0", 10, 1, false}}
 	local rateTotalDec = rateTotal
 	local rate = 10
 	local interval = 1
 	local enforce_limit = false
+	local count_limit = 0	
 	-- Determine parameters from config
     	for i,r_i_keys in ipairs(r_i) do
     	    if r_i_keys[1] == key then
@@ -129,92 +172,30 @@ function _M.limit(config)
     	    	interval = r_i_keys[3]
 		enforce_limit = r_i_keys[4]
     	    end
-	    rateTotalDec = rateTotalDec - 1;
+	    count_limit = count_limit + 1
     	end
+	rateTotalDec = rateTotalDec - (count_limit * 4)
 	
-        local current_time = ngx.now()
-        local response, error = bump_request(connection, redis_pool_size, key, rate, interval, current_time, log_level)
+        local response, error = bump_request_all(connection, redis_pool_size, key, rate, interval, rateTotal, rateTotalDec, intervalTotal, enforce_limit, log_level)
         if not response then
             return
         end
 
-        if response.count > rate then
-		if enforce_limit then
-			if overlimit(connection, key, rateTotalDec, intervalTotal, log_level) then 
-	            		local retry_after = math.floor(response.reset - current_time)
-	            		if retry_after < 0 then
-	            		    retry_after = 0
-	            		end
-	            		ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
-	            		ngx.header["Access-Control-Allow-Origin"] = "*"
-	            		ngx.header["Content-Type"] = "application/json; charset=utf-8"
-	            		ngx.header["Retry-After"] = retry_after
-	            		ngx.status = 429
-	            		ngx.say('{"status_code":25,"status_message":"Your request count (' .. response.count .. ') is over the allowed limit of ' .. rate .. '."}')
-				-- Return connection in pool
-    				local ok, error = connection:set_keepalive(60000, redis_pool_size)
-    				if not ok then
-    				    ngx.log(log_level, "failed to set keepalive: ", error)
-    				end
-	            		ngx.exit(ngx.HTTP_OK)
-            	 	else
-	        		ngx.header["X-RateLimit-Limit"] = rate
-	        		ngx.header["X-RateLimit-Remaining"] = math.floor(response.remaining)
-	        		ngx.header["X-RateLimit-Reset"] = math.floor(response.reset)
-	        		ngx.header["X-RateLimit-Limit-Total"] = rateTotalDec
-				-- Return connection in pool
-    				local ok, error = connection:set_keepalive(60000, redis_pool_size)
-    				if not ok then
-    				    ngx.log(log_level, "failed to set keepalive: ", error)
-    				end
-			end
-		else	
-	            	local retry_after = math.floor(response.reset - current_time)
-	            	if retry_after < 0 then
-	            	    retry_after = 0
-	            	end
-	            	ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
-	            	ngx.header["Access-Control-Allow-Origin"] = "*"
-	            	ngx.header["Content-Type"] = "application/json; charset=utf-8"
-	            	ngx.header["Retry-After"] = retry_after
-	            	ngx.status = 429
-	            	ngx.say('{"status_code":25,"status_message":"Your request count (' .. response.count .. ') is over the allowed limit of ' .. rate .. '."}')
-			-- Return connection in pool
-    			local ok, error = connection:set_keepalive(60000, redis_pool_size)
-    			if not ok then
-    			    ngx.log(log_level, "failed to set keepalive: ", error)
-    			end
-	            	ngx.exit(ngx.HTTP_OK)
-		end
-        else
-	    	if overlimit(connection, key, rateTotal, intervalTotal, log_level) then 
-	    		local retry_after = math.floor(response.reset - current_time)
-	    		if retry_after < 0 then
-	    		    retry_after = 0
-	    		end
-	    		ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
-	    		ngx.header["Access-Control-Allow-Origin"] = "*"
-	    		ngx.header["Content-Type"] = "application/json; charset=utf-8"
-	    		ngx.header["Retry-After"] = retry_after
-	    		ngx.status = 429
-	    		ngx.say('{"status_code":25,"status_message":"Your request count (' .. response.count .. ') is over the allowed limit of ' .. rate .. '."}')
-	        	-- Return connection in pool
-    	        	local ok, error = connection:set_keepalive(60000, redis_pool_size)
-    	        	if not ok then
-    	        	    ngx.log(log_level, "failed to set keepalive: ", error)
-    	        	end
-	    		ngx.exit(ngx.HTTP_OK)
-		else
-			ngx.header["X-RateLimit-Limit"] = rate
-			ngx.header["X-RateLimit-Remaining"] = math.floor(response.remaining)
-			ngx.header["X-RateLimit-Reset"] = math.floor(response.reset)
-			ngx.header["X-RateLimit-Limit-Total"] = rateTotalDec
-			-- Return connection in pool
-			local ok, error = connection:set_keepalive(60000, redis_pool_size)
-			if not ok then
-			    ngx.log(log_level, "failed to set keepalive: ", error)
-			end
-		end
+        if response.proceed then
+		-- Return connection in pool
+    		local ok, error = connection:set_keepalive(60000, redis_pool_size)
+    		if not ok then
+    		    ngx.log(log_level, "failed to set keepalive: ", error)
+    		end
+	else	
+		ngx.status = 429
+		ngx.say('{"status_code":25,"status_message":"Your request count is over the allowed limit."}')
+		-- Return connection in pool
+    		local ok, error = connection:set_keepalive(60000, redis_pool_size)
+    		if not ok then
+    		    ngx.log(log_level, "failed to set keepalive: ", error)
+    		end
+		ngx.exit(ngx.HTTP_OK)
         end
     else
         return
--- rsync-dev.sh
+++ rsync-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rsync -av opt/ /opt/
-rsync -av opt/ root@192.168.0.2:/opt/
+rsync -av opt/ root@app-2:/opt/
 service openresty restart
-ssh root@192.168.0.2 "service openresty restart"
+ssh root@app-2 "service openresty restart"
